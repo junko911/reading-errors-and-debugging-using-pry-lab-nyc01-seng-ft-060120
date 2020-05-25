04e17b1625c16a0949e01ad5d48fcb44b20fd4ea
Junko Tahara <junkotahara911@gmail.com>
2020-05-25 16:08:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,12 +3,8 @@ require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
-  binding.pry
-  puts "hi"
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
 end
-
-start_game('a', 'b')
